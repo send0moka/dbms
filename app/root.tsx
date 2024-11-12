4888571a9de294108e8b0511698c3b6dd79ee357
send0moka <sendomoka2@gmail.com>
2024-11-13 04:24:04 +0700

    fix(root): update global styles import to use a more descriptive variable name

--- app/root.tsx
+++ app/root.tsx
@@ -7,10 +7,10 @@ import {
   ScrollRestoration,
 } from "@remix-run/react";
 import { Analytics } from "@vercel/analytics/react";
-import styles from "~/styles/global.css";
+import globalStylesUrl from "~/styles/global.css";
 
 export function links() {
-  return [{ rel: "stylesheet", href: styles }];
+  return [{ rel: "stylesheet", href: globalStylesUrl }];
 }
 
 export function meta() {
